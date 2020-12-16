ee65e3d4795bc9a916d35c2ad5042f1cdf58defc
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-12-16 19:11:22 +0900

    <feat>: <Redux 마무리, divArr 객체형태로 변환>
     filter 함수 수정으로 차후 sort와 select 기능 구현

--- src/ddadda/actions.ts
+++ src/ddadda/actions.ts
@@ -1,6 +1,6 @@
 import { AxiosError } from 'axios';
 import { createAsyncAction } from 'typesafe-actions';
-import { mapFilterDataType } from '../utils/filter';
+import { defaultDataType } from '../utils/filter';
 
 export const DEFAULT_REQUEST = 'DEFAULT_REQUEST';
 export const DEFAULT_SUCCESS = 'DEFAULT_SUCCESS';
@@ -8,6 +8,6 @@ export const DEFAULT_FAILURE = 'DEFAULT_FAILURE';
 
 export const defaultAsync = createAsyncAction(DEFAULT_REQUEST, DEFAULT_SUCCESS, DEFAULT_FAILURE)<
   undefined,
-  mapFilterDataType,
+  defaultDataType,
   AxiosError
 >();
--- src/utils/filter.ts
+++ src/utils/filter.ts
@@ -1,31 +1,30 @@
 import * as d3 from 'd3';
 
-const divisionArr = [
-  '강남구',
-  '강동구',
-  '강북구',
-  '강서구',
-  '관악구',
-  '광진구',
-  '구로구',
-  '금천구',
-  '노원구',
-  '도봉구',
-  '동작구',
-  '동대문구',
-  '마포구',
-  '서대문구',
-  '서초구',
-  '성동구',
-  '성북구',
-  '송파구',
-  '양천구',
-  '영등포구',
-  '용산구',
-  '은평구',
-  '종로구',
-  '중구',
-  '중랑구',
+export const divisionArr = [
+  { name: '강남구', isSelect: false },
+  { name: '강동구', isSelect: false },
+  { name: '강북구', isSelect: false },
+  { name: '관악구', isSelect: false },
+  { name: '광진구', isSelect: false },
+  { name: '구로구', isSelect: false },
+  { name: '금천구', isSelect: false },
+  { name: '노원구', isSelect: false },
+  { name: '도봉구', isSelect: false },
+  { name: '동작구', isSelect: false },
+  { name: '동대문구', isSelect: false },
+  { name: '마포구', isSelect: false },
+  { name: '서대문구', isSelect: false },
+  { name: '서초구', isSelect: false },
+  { name: '성동구', isSelect: false },
+  { name: '성북구', isSelect: false },
+  { name: '송파구', isSelect: false },
+  { name: '양천구', isSelect: false },
+  { name: '영등포구', isSelect: false },
+  { name: '용산구', isSelect: false },
+  { name: '은평구', isSelect: false },
+  { name: '종로구', isSelect: false },
+  { name: '중구', isSelect: false },
+  { name: '중랑구', isSelect: false },
 ];
 
 export type ddaddaData = {
@@ -40,7 +39,7 @@ export type ddaddaData = {
 };
 
 export type defaultDataType = ReturnType<typeof filterData>;
-export type mapFilterDataType = ReturnType<typeof mapFilterData>;
+// export type mapFilterDataType = ReturnType<typeof mapFilterData>;
 
 export function filterData(array: ddaddaData[]) {
   const newData = array.map((item: ddaddaData) => {
@@ -52,18 +51,14 @@ export function filterData(array: ddaddaData[]) {
     };
     return newObj;
   });
-  return newData;
-}
-
-export function mapFilterData(array: ReturnType<typeof filterData>) {
-  const arrayGroup = d3.group(array, (d) => d.division);
-  const mapFilterArray = divisionArr.map((e: string, i: number) => {
+  const newDataGroup = d3.group(newData, (d) => d.division);
+  const mapFilterArray = divisionArr.map((e: { name: string; isSelect: boolean }, i: number) => {
     return {
-      name: e,
+      name: e.name,
       id: i,
-      divC: arrayGroup.get(e)?.length,
-      bikeC: arrayGroup
-        .get(e)
+      divC: newDataGroup.get(e.name)?.length,
+      bikeC: newDataGroup
+        .get(e.name)
         ?.map((data) => data.size)
         .reduce((acc, cur) => acc + cur, 0),
     };
@@ -71,6 +66,18 @@ export function mapFilterData(array: ReturnType<typeof filterData>) {
   return mapFilterArray;
 }
 
-export function sortFilterData(array: ReturnType<typeof mapFilterData>) {
-  return array.sort((a, b) => (a.bikeC! > b.bikeC! ? 1 : -1));
-}
+// export function mapFilterData(array: ReturnType<typeof filterData>) {
+//   const arrayGroup = d3.group(array, (d) => d.division);
+//   const mapFilterArray = divisionArr.map((e: string, i: number) => {
+//     return {
+//       name: e,
+//       id: i,
+//       divC: arrayGroup.get(e)?.length,
+//       bikeC: arrayGroup
+//         .get(e)
+//         ?.map((data) => data.size)
+//         .reduce((acc, cur) => acc + cur, 0),
+//     };
+//   });
+//   return mapFilterArray;
+// }
