0a434a4bd255aa4cb15e8dee57c4390cfd2b41f9
badbeoti <59817244+badbeoti@users.noreply.github.com>
2020-12-18 22:57:29 +0900

    <fix>: <Css fix>

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
-import { Visible } from 'react-grid-system';
 import ButtonList from './components/ButtonList';
 import Canvas from './components/Canvas';
-import ButtonListXS from './components/ButtonListXS';
-import CanvasXS from './components/CanvasXS';
 import './App.css';
 
 const AppBox = styled.div`
